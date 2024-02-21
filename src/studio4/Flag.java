a3223a05e83f2ecf42ca269f39a0c45813c680e7
bc <bc@localhost>
2024-02-21 17:22:55 -0600

    studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,20 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.rectangle(0.5, 0.5, 0.375, 0.25);
+		
+		Color PURPLE = new Color(255, 0, 255);
+		
+		StdDraw.setPenColor(PURPLE);
+		
+		StdDraw.filledRectangle(0.375, 0.5, 0.125, 0.125);
+		
+		StdDraw.setPenColor(Color.GREEN);
+		
+		StdDraw.filledRectangle(0.625, 0.5, 0.125, 0.125);
+		
+		StdDraw.setPenColor(Color.RED);
+		
+		StdDraw.filledCircle(0.5, 0.5, 0.05);
 	}
 }
